1b9ddcd434803f82d4cd959c125e063116e70a45
Lukas Bableck <lukas@bableck.dev>
2025-07-25 17:22:04 +0200

    fix translations

--- contao/languages/en/tl_content.php
+++ contao/languages/en/tl_content.php
@@ -1,9 +1,10 @@
 <?php
 
+$GLOBALS['TL_LANG']['tl_content']['content_legend'] = 'Content';
 $GLOBALS['TL_LANG']['tl_content']['llmsHeadline'] = ['Headline', 'Here you can enter a headline for the content.'];
 $GLOBALS['TL_LANG']['tl_content']['llmsText'] = ['Text', 'Here you can enter the content of the text.'];
 
-$GLOBALS['TL_LANG']['tl_content']['llmsLinkList'] = ['Link list', 'Here you can create a list of links.'];
-$GLOBALS['TL_LANG']['tl_content']['llmsLinkList_']['linkTitle'] = ['Title', 'Enter the title of the link here.'];
-$GLOBALS['TL_LANG']['tl_content']['llmsLinkList_']['linkURL'] = ['URL', 'Enter the URL of the link here.'];
-$GLOBALS['TL_LANG']['tl_content']['llmsLinkList_']['linkDetails'] = ['Details', 'Enter additional details about the link here.'];
+$GLOBALS['TL_LANG']['tl_content']['llmsLinklist'] = ['Link list', 'Here you can create a list of links.'];
+$GLOBALS['TL_LANG']['tl_content']['llmsLinklist_']['linkTitle'] = ['Title', 'Enter the title of the link here.'];
+$GLOBALS['TL_LANG']['tl_content']['llmsLinklist_']['linkURL'] = ['URL', 'Enter the URL of the link here.'];
+$GLOBALS['TL_LANG']['tl_content']['llmsLinklist_']['linkDetails'] = ['Details', 'Enter additional details about the link here.'];
